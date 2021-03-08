4d3fe75467a863099cac44d979d606826ff5bba7
sdauuse <751648872@qq.com>
2021-03-08 15:58:04 +0800

    新增springconfig

---
+++ cloud-config-center-3344/src/main/java/com/miao/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.miao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author miaoyin
+ * @date 2021/3/8 - 14:40
+ * @commet:
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
